2def3de28a283296a56d4bf1f626d514605a24cc
saimohanchebrolu <158805695+saimohanchebrolu@users.noreply.github.com>
2026-07-04 08:09:26 +0530

    Adding app/products to get data from JSON
    Removed unnecessary imports and routes from app.py.

--- backend/app.py
+++ backend/app.py
@@ -23,29 +23,4 @@ def get_products():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)from flask import Flask, jsonify
-from flask_cors import CORS
-import json
-
-app = Flask(__name__)
-
-CORS(app)
-
-
-@app.route("/")
-def home():
-    return {
-        "message": "Amazon Backend API is running successfully"
-    }
-
-
-@app.route("/api/products")
-def get_products():
-    with open("products.json", "r", encoding="utf-8") as file:
-        products = json.load(file)
-
-    return jsonify(products)
-
-
-if __name__ == "__main__":
     app.run(debug=True)
