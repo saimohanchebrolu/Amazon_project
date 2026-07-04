720694513b1ce269fedfbf907fa3fd55c07b21e3
saimohanchebrolu <158805695+saimohanchebrolu@users.noreply.github.com>
2026-07-04 08:24:31 +0530

    Add configuration settings for MySQL database

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Config:
+
+    SECRET_KEY = "amazon-secret-key"
+
+    MYSQL_HOST = "localhost"
+
+    MYSQL_USER = "root"
+
+    MYSQL_PASSWORD = "password"
+
+    MYSQL_DB = "amazon_db"
