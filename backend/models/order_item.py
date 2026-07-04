c91b4092f9521763fc8253b49a52109850d80489
saimohanchebrolu <158805695+saimohanchebrolu@users.noreply.github.com>
2026-07-04 09:27:29 +0530

    Create order_item.py

---
+++ backend/models/order_item.py
@@ -0,0 +1,32 @@
+from database import db
+
+
+class OrderItem(db.Model):
+
+    __tablename__ = "order_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    order_id = db.Column(
+        db.Integer,
+        db.ForeignKey("orders.id"),
+        nullable=False
+    )
+
+    product_id = db.Column(
+        db.Integer,
+        db.ForeignKey("products.id"),
+        nullable=False
+    )
+
+    quantity = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    price = db.Column(
+        db.Float,
+        nullable=False
+    )
+
+    product = db.relationship("Product")
