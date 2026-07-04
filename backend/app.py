efa07f211c498d005b588a35e06fcdb40021c79b
saimohanchebrolu <158805695+saimohanchebrolu@users.noreply.github.com>
2026-07-04 09:10:35 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,34 @@
 from flask import Flask
 from flask_cors import CORS
 
+from config import Config
+from database import db
+
 from routes.products import product_bp
+
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
 CORS(app)
 
-app.register_blueprint(product_bp)
+db.init_app(app)
+
+app.register_blueprint(product_bp, url_prefix="/api")
 
 
 @app.route("/")
 def home():
+
     return {
-        "message": "Amazon Backend API is running successfully"
+        "message": "Amazon Backend API Running Successfully"
     }
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
