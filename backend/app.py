1ef6ad07b9f0771d90be5cb7105b7dc9d1d098bb
saimohanchebrolu <158805695+saimohanchebrolu@users.noreply.github.com>
2026-07-04 08:27:01 +0530

    Refactor app.py by cleaning up imports and routes
    Removed unused imports and the get_products route.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-import json
+
+from routes.products import product_bp
 
 app = Flask(__name__)
 
 CORS(app)
+
+app.register_blueprint(product_bp)
 
 
 @app.route("/")
@@ -12,14 +15,6 @@ def home():
     return {
         "message": "Amazon Backend API is running successfully"
     }
-
-
-@app.route("/api/products")
-def get_products():
-    with open("products.json", "r", encoding="utf-8") as file:
-        products = json.load(file)
-
-    return jsonify(products)
 
 
 if __name__ == "__main__":
